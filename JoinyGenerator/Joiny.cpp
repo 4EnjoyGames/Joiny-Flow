d4498e1fdeed19260ffe2aedaa1b2b072f9b6368
Diana Sirenko <dianasirenko@gmail.com>
2014-01-28 17:48:53 +0200

    make flow code readable

--- JoinyGenerator/Joiny.cpp
+++ JoinyGenerator/Joiny.cpp
@@ -31,40 +31,60 @@ public:
     typedef map<unsigned int, unsigned int> Scores;
     Scores _scores;
 
-    NumberLink(const Distance width, const Distance height):
-        width_(width), height_(height), size_((CellKey)width * height),
-        cell_x_(size_), cell_y_(size_), table_(size_),
-        keys_(size_), mates_(size_), start_(size_ + 1),
-        connected_x_(size_), connected_y_(size_), memo_(size_) {}
+    NumberLink(const Distance width,
+               const Distance height):
+        width_(width),
+        height_(height),
+        size_((CellKey)width * height),
+        cell_x_(size_),
+        cell_y_(size_),
+        table_(size_),
+        keys_(size_),
+        mates_(size_),
+        start_(size_ + 1),
+        connected_x_(size_),
+        connected_y_(size_),
+        memo_(size_) {}
 
-    void Initialize() {
+    void Initialize()
+    {
         // Initialize mates
-        for (CellKey cell_key = 0; cell_key < size_; cell_key++) {
+        for (CellKey cell_key = 0; cell_key < size_; cell_key++)
+        {
             mates_[cell_key] = cell_key;
         }
 
         // Generates references: (x, y) <=> CellKey
         Distance x = 0, y = 0;
         CellKey cell_key = 0;
-        while (true) {
+
+        while (true)
+        {
             CellPosition position = (CellPosition)y * width_ + x;
             cell_x_[cell_key] = x;
             cell_y_[cell_key] = y;
             keys_[position] = cell_key;
             cell_key++;
-            if (cell_key == size_) break;
-            do {
+
+            if (cell_key == size_)
+                break;
+            do
+            {
                 x--;
                 y++;
-                if (x < 0) {
+                if (x < 0)
+                {
                     x = y;
                     y = 0;
                 }
-            } while (x < 0 || width_ <= x || y < 0 || height_ <= y);
+            }
+            while (x < 0 || width_ <= x || y < 0 || height_ <= y);
         }
 
+
         // Pre-compute CellKey to look back for every cell
-        for (CellKey i = 0; i < size_; i++) {
+        for (CellKey i = 0; i < size_; i++)
+        {
             Distance x = cell_x_[i], y = cell_y_[i];
             start_[i] = 0 < y ? GetCellKey(x, y - 1) :
                                 (0 < x ? GetCellKey(x - 1, y) : 0);
@@ -75,76 +95,108 @@ public:
 
     // Returns the reference of the number written in the cell (x,y).
     CellNumber& Cell(const Distance x, const Distance y)
-    { return table_[GetCellKey(x, y)]; }
+    {
+        return table_[GetCellKey(x, y)];
+    }
+
     // Returns the key of the special order for the coordinate (x,y).
     CellKey GetCellKey(const Distance x, const Distance y) const
-    { return keys_[(CellPosition)y * width_ + x]; }
+    {
+        return keys_[(CellPosition)y * width_ + x];
+    }
 
-    double Solve(const CellKey cell_key = 0) {
+    double Solve(const CellKey cell_key = 0)
+    {
         if (cell_key == 0) solved_ = false;
         // See the newly fixed cells
-        if (0 < cell_key) {
+        if (0 < cell_key)
+        {
             for (CellKey hidden = start_[cell_key - 1];
-                 hidden < start_[cell_key]; hidden++) {
-                if (table_[hidden] == 0) {
+                 hidden < start_[cell_key]; hidden++)
+            {
+                if (table_[hidden] == 0)
+                {
                     // Return if the empty cell has an end
-                    if (mates_[hidden] != -1 && mates_[hidden] != hidden) return 0.0;
-                } else {
+                    if (mates_[hidden] != -1 && mates_[hidden] != hidden)
+                        return 0.0;
+                }
+                else
+                {
                     // Return if the numbered cell has no line
-                    if (mates_[hidden] == hidden) return 0.0;
+                    if (mates_[hidden] == hidden)
+                        return 0.0;
                 }
             }
         }
 
         // If all the cells are filled
-        if (cell_key == size_) {
-            //if (!solved_) {
+        if (cell_key == size_)
+        {
             Print();
-            //    solved_ = true;
-            //}
             return 1.0;
         }
         else
         {
             Print();
         }
-        //Print();
+
         // Connect successive cells if this sequence of mates has not been seen
         const vector<CellKey> mate_tuple(mates_.begin() + start_[cell_key],
                                          mates_.begin() + cell_key);
         const Hash mate_hash = GetHash(mate_tuple);
-        if (!memo_[cell_key].count(mate_hash)) {
+        if (!memo_[cell_key].count(mate_hash))
+        {
             memo_[cell_key][mate_hash] = Connect(cell_key);
         }
         return memo_[cell_key][mate_hash];
     }
 
-    double Connect(const CellKey cell_key) {
+    double Connect(const CellKey cell_key)
+    {
         double solution_count = 0.0;
-        Distance x = cell_x_[cell_key], y = cell_y_[cell_key];
-        CellKey left_cell_key = -1, up_cell_key = -1;
-        if (0 < x) left_cell_key = GetCellKey(x - 1, y);
-        if (0 < y) up_cell_key = GetCellKey(x, y - 1);
+
+        Distance x = cell_x_[cell_key];
+        Distance y = cell_y_[cell_key];
+
+        CellKey left_cell_key = -1;
+        CellKey up_cell_key = -1;
+
+        if (0 < x)
+            left_cell_key = GetCellKey(x - 1, y);
+
+        if (0 < y)
+            up_cell_key = GetCellKey(x, y - 1);
+
         size_t revert_point = mate_stack_.size();
+
         // Connect the cell with nothing
         solution_count += Solve(cell_key + 1);
+
         // Connect the cell with the upper cell
-        if (0 <= up_cell_key) {
-            if (UniteMates(cell_key, up_cell_key)) {
+        if (0 <= up_cell_key)
+        {
+            if (UniteMates(cell_key, up_cell_key))
+            {
                 connected_y_[cell_key] = true;
                 solution_count += Solve(cell_key + 1);
                 connected_y_[cell_key] = false;
             }
             RevertMates(revert_point);
         }
+
         // Connect the cell with the left cell
-        if (0 <= left_cell_key) {
-            if (UniteMates(cell_key, left_cell_key)) {
+        if (0 <= left_cell_key)
+        {
+            if (UniteMates(cell_key, left_cell_key))
+            {
                 connected_x_[cell_key] = true;
                 solution_count += Solve(cell_key + 1);
+
                 // Connect the cell with the upper and the left cells
-                if (0 <= up_cell_key) {
-                    if (UniteMates(cell_key, up_cell_key)) {
+                if (0 <= up_cell_key)
+                {
+                    if (UniteMates(cell_key, up_cell_key))
+                    {
                         connected_y_[cell_key] = true;
                         solution_count += Solve(cell_key + 1);
                         connected_y_[cell_key] = false;
@@ -159,9 +211,12 @@ public:
 
     std::vector<bool> _traversed;
 
-    unsigned int traverse(const unsigned int x, const unsigned int y, unsigned int path_lenth)
+    unsigned int traverse(const unsigned int x,
+                          const unsigned int y,
+                          unsigned int path_lenth)
     {
         _traversed[GetCellKey(x, y)] = true;
+
         if(path_lenth > 0 && table_[GetCellKey(x, y)])
         {
             unsigned int path = path_lenth;
@@ -172,19 +227,23 @@ public:
 
 
         //Next go to left?
-        if(x > 0 && connected_x_[GetCellKey(x, y)] && !_traversed[GetCellKey(x-1, y)])
+        if(x > 0 && connected_x_[GetCellKey(x, y)] &&
+                !_traversed[GetCellKey(x-1, y)])
             return traverse(x-1, y, path_lenth+1);
 
         //Next go to top?
-        if(y > 0 && connected_y_[GetCellKey(x, y)] && !_traversed[GetCellKey(x, y-1)])
+        if(y > 0 && connected_y_[GetCellKey(x, y)] &&
+                !_traversed[GetCellKey(x, y-1)])
             return traverse(x, y-1, path_lenth+1);
 
         //Next go to right?
-        if(x+1 < width_ && connected_x_[GetCellKey(x+1, y)] && !_traversed[GetCellKey(x+1, y)])
+        if(x+1 < width_ && connected_x_[GetCellKey(x+1, y)] &&
+                !_traversed[GetCellKey(x+1, y)])
             return traverse(x+1, y, path_lenth+1);
 
         //Next go bottom?
-        if(y+1 < height_ && connected_y_[GetCellKey(x, y+1)] && !_traversed[GetCellKey(x, y+1)])
+        if(y+1 < height_ && connected_y_[GetCellKey(x, y+1)] &&
+                !_traversed[GetCellKey(x, y+1)])
             return traverse(x, y+1, path_lenth+1);
 
         return 0;
@@ -310,48 +369,78 @@ public:
 private:
     // Hash key to identify a sequence of CellKeys
     typedef pair<long long, long long> Hash;
+
     // The number of cells on the board
     CellKey size_;
+
     // History of modification
     stack< pair<CellKey, CellKey> > mate_stack_;
+
     // Map from a CellKey to a coordinate
-    vector<Distance> cell_x_, cell_y_;
+    vector<Distance> cell_x_;
+    vector<Distance> cell_y_;
+
     // Store the numbers in the cells
     vector<CellNumber> table_;
-    vector<CellKey> keys_, mates_, start_;
+    vector<CellKey> keys_;
+    vector<CellKey> mates_;
+    vector<CellKey> start_;
+
     // Description of links on the board
-    vector<bool> connected_x_, connected_y_;
+    vector<bool> connected_x;
+    vector<bool> connected_y_;
+
     // Hash table to identify a sequence of CellKeys
     vector< map<Hash, double> > memo_;
+
     // Flag to know whether the problem has been solved
     bool solved_;
 
     // Get a hash key based on the XorShift algorithm
-    Hash GetHash(const vector<CellKey> &cell_keys) {
-        unsigned int x = 123456789, y = 362436069, z = 521288629, w = 88675123;
-        for (int i = 0; i < (int)cell_keys.size(); i++) {
-            unsigned int t = (x ^ (x << 11)); x = y; y = z; z = w;
-            w = (w ^ (w >> 19)) ^ (t ^ (t >> 8)) + (unsigned int)cell_keys[i];
+    Hash GetHash(const vector<CellKey> &cell_keys)
+    {
+        unsigned int x = 123456789;
+        unsigned int y = 362436069;
+        unsigned int z = 521288629;
+        unsigned int w = 88675123;
+
+        for (int i = 0; i < (int)cell_keys.size(); i++)
+        {
+            unsigned int t = (x ^ (x << 11));
+            x = y;
+            y = z;
+            z = w;
+
+            w = (w ^ (w >> 19)) ^ (t ^ (t >> 8))
+                    + (unsigned int)cell_keys[i];
         }
+
         Hash h;
         h.first = ((unsigned long long)x << 32) | y;
         h.second = ((unsigned long long)z << 32) | w;
         return h;
     }
 
+
     // Change the table of mates in adding the history
-    int ChangeMates(const CellKey cell_key, const CellKey cell_value) {
+    int ChangeMates(const CellKey cell_key,
+                    const CellKey cell_value)
+    {
         int last_stack_size = mate_stack_.size();
         CellKey last_value = mates_[cell_key];
-        if (last_value != cell_value) {
+
+        if (last_value != cell_value)
+        {
             mate_stack_.push(make_pair(cell_key, last_value));
             mates_[cell_key] = cell_value;
         }
+
         return last_stack_size;
     }
 
     // Revert the table of mates using the history
-    void RevertMates(const size_t stack_size) {
+    void RevertMates(const size_t stack_size)
+    {
         for (; stack_size < mate_stack_.size(); mate_stack_.pop())
             mates_[mate_stack_.top().first] = mate_stack_.top().second;
     }
@@ -359,25 +448,36 @@ private:
     // Connects cell_key1 and cell_key2 by a line. Returns false if it cannot
     // connect the cells because of constraints. The table of mates must be
     // reverted even if the cells cannot be connect correctly.
-    bool UniteMates(const CellKey cell_key1, const CellKey cell_key2) {
+
+    bool UniteMates(const CellKey cell_key1, const CellKey cell_key2)
+    {
         CellKey end1 = mates_[cell_key1], end2 = mates_[cell_key2];
+
         // Cannot connect any branch
-        if (end1 == -1 || end2 == -1) return false;
+        if (end1 == -1 || end2 == -1)
+            return false;
+
         // Avoid making a cycle
-        if (cell_key1 == end2 && cell_key2 == end1) return false;
+        if (cell_key1 == end2 && cell_key2 == end1)
+            return false;
+
         // Change states of mates to connect cell_key1 and cell_key2
         ChangeMates(cell_key1, -1);
         ChangeMates(cell_key2, -1);
         ChangeMates(end1, end2);
         ChangeMates(end2, end1);
+
         // Check three constraints:
         //   1. cell_key1 must not be a branch if cell_key1 is numbered,
         //   2. cell_key2 must not be a branch if cell_key2 is numbered,
         //   3. Different numbers cannot be connected.
-        if (mates_[cell_key1] == -1 && 0 < table_[cell_key1]) return false;
-        if (mates_[cell_key2] == -1 && 0 < table_[cell_key2]) return false;
+        if (mates_[cell_key1] == -1 && 0 < table_[cell_key1])
+            return false;
+        if (mates_[cell_key2] == -1 && 0 < table_[cell_key2])
+            return false;
         if (0 < table_[end1] && 0 < table_[end2] &&
-                table_[end1] != table_[end2]) return false;
+                table_[end1] != table_[end2])
+            return false;
         return true;
     }
 };
