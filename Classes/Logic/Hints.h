847f6055ee39267550aa1ad2d63470d701180967
Diana Sirenko <dianasirenko@gmail.com>
2014-02-05 19:08:50 +0200

    #6 make Hints class no Singletone + save Hintsnow
    ! if you need more hints change Hint number in constructor of
    Logic/LevelManager - line12

--- Classes/Logic/Hints.h
+++ Classes/Logic/Hints.h
@@ -7,16 +7,18 @@
 class Hints
 {
 public:
-    static Hints* getInstance( );
-
-    void newLevel();
+    //static Hints* getInstance( );
+    Hints(const JoinyLevel *level, FlowGame *flow_game);
+    Hints();
 
     //return we show the hint succesfully or not
-    bool showHint(const JoinyLevel* level, FlowGame* flow_game);
-    void deleteHint(const JoinyLevel *level, FlowGame *flow_game);
+    bool showHint();
+    void deleteHint();
 
     const unsigned int getHintNumber();
     void useHint();
+    void decreaseHintNumber(const unsigned int num=1);
+    void increaseHintNumber(const unsigned int num=1);
     bool hasHint();
 
 
@@ -26,26 +28,22 @@ public:
                        const std::vector<FlowPoint>& vec) const;
 
 private:
-    Hints();
-    Hints(Hints const&);              // don't Implement
-    void operator=(Hints const&); // don't implement
+    //Hints();
+    //Hints(Hints const&);              // don't Implement
+    //void operator=(Hints const&); // don't implement
 
 
 
-    static Hints* _instance;
+    //static Hints* _instance;
 
     const JoinyLevel *_level;
     FlowGame *_flow_game;
 
     unsigned int _hint_number;
 
-    //save for each level id the hint id, which we must show next time
-    typedef unsigned int PathID;
-    std::map<JoinyLevelID, PathID> _order;
-
-
     //save all pathes, which we hinted with their LevelID
-    std::map< JoinyLevelID, UsedPathes > _saves;
+    UsedPathes _saves;
+    //std::map< JoinyLevelID, UsedPathes > _saves;
 
     friend class LevelManager;
     void setHintNumber(const unsigned int num);
