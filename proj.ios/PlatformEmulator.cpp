49e8936a1b853de051876365ab3111fe223221e0
Andriy Chaika <andriy@chaika.in.ua>
2014-02-06 20:25:17 +0200

    close #43 iOS ADLanguage support

--- proj.ios/PlatformEmulator.cpp
+++ proj.ios/PlatformEmulator.cpp
@@ -9,7 +9,7 @@
 
 #include <ADLib/PlatformImpl/ADBrowser_None.hpp>
 #include <ADLib/PlatformImpl/ADStatistics_None.hpp>
-#include <ADLib/PlatformImpl/ADLanguage_Default.hpp>
+//#include <ADLib/PlatformImpl/ADLanguage_Default.hpp>
 #include <ADLib/PlatformImpl/ADNotification_None.hpp>
 #include <ADLib/PlatformImpl/ADInfo_Default.hpp>
 #include <ADLib/PlatformImpl/ADInApp_Emulator.hpp>
