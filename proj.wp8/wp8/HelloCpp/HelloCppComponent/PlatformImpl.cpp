ce915ad049320b65918bca2db6e2aa30414fe8e3
Andriy Chaika <andriy+en@chaika.in.ua>
2014-03-21 14:29:39 +0200

    wp8 ADNotification

--- proj.wp8/wp8/HelloCpp/HelloCppComponent/PlatformImpl.cpp
+++ proj.wp8/wp8/HelloCpp/HelloCppComponent/PlatformImpl.cpp
@@ -3,6 +3,6 @@
 #include <ADLib\PlatformImpl\ADInApp_None.hpp>
 //#include <ADLib\PlatformImpl\ADInfo_Default.hpp>
 //#include <ADLib\PlatformImpl\ADLanguage_Default.hpp>
-#include <ADLib\PlatformImpl\ADNotification_None.hpp>
+//#include <ADLib\PlatformImpl\ADNotification_None.hpp>
 #include <ADLib\PlatformImpl\ADStatistics_None.hpp>
 #include <ADLib\PlatformImpl\ADVirtualCurrency_None.hpp>
